36316c1e7d57879003f080ba7007d1a60dab6e7c
Lakhan <lakhansoni126@gmail.com>
2022-10-10 13:12:59 +0530

    start

---
+++ clone/src/App.js
@@ -0,0 +1,22 @@
+import{ BrowserRouter as Router, Switch, Route} from'react-router-dom';
+import Login from "./components/Login";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      <Router>
+      
+        <Switch>
+           <Route exact path = "/">
+            <login/>
+           </Route>
+        </Switch>
+      
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ clone/src/components/Login.js
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+const Login =(props) =>{
+return <div>login</div>
+};
