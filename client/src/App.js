e7475cf0deb0c2d0035473025f4671f4d7324358
Lakhan <lakhansoni126@gmail.com>
2023-01-13 14:34:49 +0530

    Revert "12-01-23"
    This reverts commit a30f48dda3b23575d83d7d6ca06e3c2b14eb036d.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 
-import './/CSS/App';
-import Home from './Pages/Home';
+import './App.css';
 
 function App() {
   return (
-    <div className='App' >
-      <div className="blur" style={{top:'-18%', right:'0'} }></div>
-      <div className="blur" style={{top:'36%' ,left: '--8rem'} }></div>
-
-      <Home/>
-
+    <div >
+     sad
     </div>
   );
 }
