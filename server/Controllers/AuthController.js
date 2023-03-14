5c1b37301c1cdb1cfa95258d20a86e32ea09e0a9
Lakhan <lakhansoni126@gmail.com>
2023-03-14 21:08:15 +0530

    13-03-2023

---
+++ server/Controllers/AuthController.js
@@ -0,0 +1,40 @@
+import UserModel from "../Models/userModel.js";
+import bcrypt from 'bcrypt';
+
+export const registerUser=async(req,res)=>{
+    const {username, password, firstname,lastname} =req.body;
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword= await bcrypt.hash(password,salt)
+
+    const newUser = new UserModel({username,password:hashedPassword,firstname,lastname})
+
+
+    try {
+        await newUser.save()
+        res.status(200).json(newUser)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+
+export const loginUser= async(req, res)=>{
+    const {username,password}=req.body
+
+    try {
+        const user = await UserModel.findOne({username : username})
+
+        if(user)
+        {
+            const validity = await bcrypt.compare(password, user.password)
+
+            validity? res.status(200).json(user): res.status(400).json("wrong password ");
+        }
+        else{
+            res.status(404).json("user does not exists")
+        }
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose, { connect } from 'mongoose';
+import dotenv from 'dotenv';
+import AuthRoute from './Routes/AuthRoute.js'
+
+
+//
 
 const app = express();
-app.use(bodyParser.json({limit: '30mb', entended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', entended: true}));
+app.use(bodyParser.json({ limit: '30mb', entended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', entended: true }));
 
-mongoose.connect("mongodb+srv://lakhan:567894321ls@cluster0.hcx2kqb.mongodb.net/SocialMediaApp?retryWrites=true&w=majority").
-then(()=>app.listen(7000,()=>console.log("listening")));
+
+dotenv.config()
+mongoose
+    .connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+
+    .then(() => app.listen(process.env.port, () => console.log("listening")))
+
+    .catch((error)=>console.log(error))
+ 
+
+app.use('/auth', AuthRoute)
+
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    resp.send(req.result);
+});
+
+
+
+app.post("/login", async (req, resp) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user)
+        } else {
+            resp.send({ result: "no user found" })
+        }
+    } else { resp.send({ result: "no user found" }) }
+
+});
